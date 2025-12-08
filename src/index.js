1cc4fb93b046a048789dfea8183e50342b8142e0
Hugh Chatfield <hugh.chatfield@Hughs-MacBook-Pro.local>
2025-12-08 14:16:36 +0000

    hash

--- src/index.js
+++ src/index.js
@@ -5,22 +5,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; // <-- Add this import
-import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom'; // add this
 
-const theme = createTheme();
-
-window.CESIUM_BASE_URL = "/cesium";
-
- const root = ReactDOM.createRoot(document.getElementById('root'));
- root.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* <-- Wrap App in BrowserRouter */}
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-     </BrowserRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
- );
+);
+
+reportWebVitals();
 
